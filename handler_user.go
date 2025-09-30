b6d235333391a25dbda8e7fe9f9ca9463f8fde28
mvusic07 <thinkman405@gmail.com>
2025-09-30 19:49:12 +0200

    postss

--- handler_user.go
+++ handler_user.go
@@ -42,3 +42,16 @@ func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, 
 	respondWithJSON(w, 200, databaseUserToUser(user))
 
 }
+
+func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  10,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error getting posts: %v", err))
+	}
+
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
+
+}
--- models.go
+++ models.go
@@ -78,3 +78,39 @@ func databaseFeedsFollowsToFeedsFollows(feedflw []database.FeedFollow) []FeedFol
 	}
 	return feeds
 }
+
+type Post struct {
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title"`
+	Description *string   `json:"description"`
+	PublishedAt time.Time `json:"published_at"`
+	Url         string    `json:"url"`
+	FeedID      uuid.UUID `json:"feed_id"`
+}
+
+func databasePostsToPost(post database.Post) Post {
+	var description *string
+	if post.Description.Valid {
+		description = &post.Description.String
+	}
+	return Post{
+		ID:          post.ID,
+		CreatedAt:   post.CreatedAt,
+		UpdatedAt:   post.UpdatedAt,
+		Title:       post.Title,
+		Description: description,
+		PublishedAt: post.PublishedAt,
+		Url:         post.Url,
+		FeedID:      post.FeedID,
+	}
+}
+
+func databasePostsToPosts(dbposts []database.Post) []Post {
+	posts := []Post{}
+	for _, post := range dbposts {
+		posts = append(posts, databasePostsToPost(post))
+	}
+	return posts
+}
